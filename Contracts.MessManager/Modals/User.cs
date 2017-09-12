7058569b342bb5eed99978cb6227ae1656336239
codeJith <abijeeth08@gmail.com>
2017-09-12 22:50:41 +0530

    Added User modal class

---
+++ Contracts.MessManager/Modals/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contracts.MessManager.Modals
+{
+    public class User
+    {
+        public int profileID { get; set; }
+
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+
+        public string Designation { get; set; }
+
+        public string Email { get; set; }
+
+        public int ContactNumber { get; set; }
+
+    }
+}
