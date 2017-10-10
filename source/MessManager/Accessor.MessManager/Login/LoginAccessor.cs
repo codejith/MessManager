8eeaaad90715176ff025627858da4a53317a2810
codeJith <abijeeth08@gmail.com>
2017-10-11 00:17:55 +0530

    Raw Login Accessor  added

---
+++ source/MessManager/Accessor.MessManager/Login/LoginAccessor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Contracts.MessManager.Modals;
+
+namespace Accessor.MessManager.Login
+{
+    class LoginAccessor
+    {
+        public LoginAccessor()
+        {
+
+        }
+
+        public User getUserData()
+        {
+            //TODO
+
+            return new User();
+        }
+    }
+}
